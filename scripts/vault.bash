8fb39e2cd70c1efd2d7b040e9bdcb705b1172fc3
xnoto <steven@makeitwork.cloud>
2025-09-26 14:08:43 -0600

    Attempting new fix

--- scripts/vault.bash
+++ scripts/vault.bash
@@ -1,3 +1,18 @@
 #!/bin/bash
-which sops >/dev/null 2>&1 || exit 0
+# Function to check script arguments for --vault-password-file
+vault_file_arg() {
+    for arg in "$@"; do
+        if [[ "$arg" == --vault-password-file=* ]]; then
+            echo "${arg#--vault-password-file=}"
+            return
+        fi
+        # Support split option style: --vault-password-file FILE
+        if [[ "$arg" == --vault-password-file ]]; then
+            next_is_file=true
+        elif [[ "$next_is_file" == true ]]; then
+            echo "$arg"
+            return
+        fi
+    done
+}
 sops decrypt secrets/secrets.yaml | grep vault_password | awk '{print $2}'
