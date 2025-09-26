d44bbda61a6d10e16b9497a44700ab3e5fd4a0b6
xnoto <steven@makeitwork.cloud>
2025-09-26 14:32:22 -0600

    Adding cicd and Makefile for commits

--- scripts/vault.bash
+++ scripts/vault.bash
@@ -1,25 +1,2 @@
 #!/bin/bash
-# Function to check script arguments for --vault-password-file
-vault_file_arg() {
-    for arg in "$@"; do
-        if [[ "$arg" == --vault-password-file=* ]]; then
-            echo "${arg#--vault-password-file=}"
-            return
-        fi
-        # Support split option style: --vault-password-file FILE
-        if [[ "$arg" == --vault-password-file ]]; then
-            next_is_file=true
-        elif [[ "$next_is_file" == true ]]; then
-            echo "$arg"
-            return
-        fi
-    done
-}
-vault_arg=$(vault_file_arg "$@")
-
-if [[ -n "$vault_arg" ]]
-then
-    cat "${vault_cfg:-$vault_arg}"
-    exit 0
-fi
 sops decrypt secrets/secrets.yaml | grep vault_password | awk '{print $2}'
